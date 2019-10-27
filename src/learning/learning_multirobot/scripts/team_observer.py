18587c56d2e88bfea617f91f831499c29bea0bfe
Davide-fisso <marcandavi@gmail.com>
2019-10-27 18:09:14 +0100

    observer __init__ now takes team_size argument

--- src/learning/learning_multirobot/scripts/team_observer.py
+++ src/learning/learning_multirobot/scripts/team_observer.py
@@ -5,13 +5,11 @@ from learning_multirobot.msg import RobotStatus
 from std_msgs.msg import String
 
 
-TEAM_SIZE = 4
-
-
 class Observer(object):
-    members_ready = 0
+    members_ready = team_size = 0
 
-    def __init__(self):
+    def __init__(self, team_size):
+        self.team_size = team_size
         rospy.init_node('observer')
 
     def wait_for_members(self, listen_topic):
@@ -20,24 +18,23 @@ class Observer(object):
     def update_team_status(self, msg):
         self.members_ready += 1
 
-        if self.members_ready == TEAM_SIZE:
+        if self.members_ready == self.team_size:
             rospy.loginfo('Observer: all members are ready, starting mission!')
             self.start_mission('start_mission')
         else:
             rospy.loginfo('Observer: %s members are ready, waiting...' % self.members_ready)
 
-    @staticmethod
-    def start_mission(mission_topic):
+    def start_mission(self, mission_topic):
         pub = rospy.Publisher(mission_topic, String, queue_size=5)
 
-        while pub.get_num_connections() < TEAM_SIZE:
+        while pub.get_num_connections() < self.team_size:
             rospy.sleep(0.1)
 
         pub.publish('Ready')
 
 
 if __name__ == '__main__':
-    observer = Observer()
+    observer = Observer(team_size=4)
     observer.wait_for_members(listen_topic='team_status')
 
     rospy.spin()
