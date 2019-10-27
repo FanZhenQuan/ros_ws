2da0d9b1976c82935e86018bea5a09331268c119
Davide-fisso <marcandavi@gmail.com>
2019-10-27 18:00:43 +0100

    multirobot_mission works with dummy mission

--- src/learning/learning_multirobot/scripts/team_member.py
+++ src/learning/learning_multirobot/scripts/team_member.py
@@ -3,31 +3,44 @@
 import rospy
 import sys
 from learning_multirobot.msg import RobotStatus
-from std_msgs.msg import Header
+from std_msgs.msg import Header, String
 
 
 class Robot(object):
     id = None
 
     def __init__(self):
-        self.id = sys.argv[1]
-        rospy.init_node('robot' + self.id)
+        rospy.init_node('robot' + sys.argv[1])
+        self.id = int(sys.argv[1])
 
     def publish_ready_state(self, topic):
         header = Header()
         header.stamp = rospy.Time.now()
 
-        msg = RobotStatus()
-        msg.is_ready = True
-        msg.robot_id = self.id
-        msg.header = header
+        ready_msg = RobotStatus()
+        ready_msg.is_ready = True
+        ready_msg.robot_id = self.id
+        ready_msg.header = header
 
         pub = rospy.Publisher(topic, RobotStatus, queue_size=10)
-        pub.publish(msg)
+
+        # finche' non c'e' un subscriber, aspetta
+        while pub.get_num_connections() == 0:
+            rospy.sleep(0.5)
+
+        pub.publish(ready_msg)
+
+    def wait_for_start(self, topic):
+        rospy.Subscriber(topic, String, self.start_mission)
+
+    def start_mission(self, msg):
+        if msg.data == 'Ready':
+            rospy.loginfo('Robot %s: started mission' % self.id)
 
 
 if __name__ == '__main__':
     robot = Robot()
     robot.publish_ready_state(topic='team_status')
+    robot.wait_for_start(topic='start_mission')
 
     rospy.spin()
--- src/learning/learning_multirobot/scripts/team_observer.py
+++ src/learning/learning_multirobot/scripts/team_observer.py
@@ -1,3 +1,41 @@
 #!/usr/bin/env python
 
 import rospy
+from learning_multirobot.msg import RobotStatus
+from std_msgs.msg import String
+
+
+class Observer(object):
+    members_ready = team_size = 0
+
+    def __init__(self, team_size):
+        self.team_size = team_size
+        rospy.init_node('observer')
+
+    def wait_for_members(self, listen_topic):
+        rospy.Subscriber(listen_topic, RobotStatus, self.update_team_status)
+
+    def update_team_status(self, msg):
+        self.members_ready += 1
+
+        if self.members_ready == TEAM_SIZE:
+            rospy.loginfo('Observer: all members are ready, starting mission!')
+            self.start_mission('start_mission')
+        else:
+            rospy.loginfo('Observer: %s members are ready, waiting...' % self.members_ready)
+
+    @staticmethod
+    def start_mission(mission_topic):
+        pub = rospy.Publisher(mission_topic, String, queue_size=5)
+
+        while pub.get_num_connections() < TEAM_SIZE:
+            rospy.sleep(0.1)
+
+        pub.publish('Ready')
+
+
+if __name__ == '__main__':
+    observer = Observer(team_size=4)
+    observer.wait_for_members(listen_topic='team_status')
+
+    rospy.spin()
