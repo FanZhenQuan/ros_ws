9f263c3a701d4a8b3efc1ae0ad0f74faec54b8ab
Davide - acer <marcandavi@gmail.com>
2019-11-11 12:21:26 +0100

    move_base params adjust
    commentati i valori predefiniti dalla documentazione, mentre sono stati lasciati i valori che ho attualmente in uso

--- src/my_fun/scripts/test1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import rospy
-
-if __name__ == '__main__':
-    rospy.init_node('found_thing')
-
-    while not rospy.is_shutdown():
-        rospy.loginfo('lol')
--- src/my_fun/scripts/test2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import rospy
-
-if __name__ == "__main__":
-    rospy.init_node('not_found_thing')
-
-    while not rospy.is_shutdown():
-        rospy.loginfo('sas')
