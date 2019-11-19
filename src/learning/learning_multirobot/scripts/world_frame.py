264f51c9ef72201efa7a4a8407d36cb2b1288b15
Davide-fisso <marcandavi@gmail.com>
2019-11-19 10:49:32 +0100

    world frame replaced by static map frame
    il frame world era superfluo, rimpiazzato con il classico frame map. i parametri di map_merge lo accettano comunque

--- src/learning/learning_multirobot/scripts/world_frame.py
+++ src/learning/learning_multirobot/scripts/world_frame.py
@@ -7,14 +7,26 @@ import tf
 if __name__ == '__main__':
     rospy.init_node('fixed_tf_broadcaster')
     br = tf.TransformBroadcaster()
-    rate = rospy.Rate(20.0)
+    rate = rospy.Rate(10.0)
     
     robot_map_frame = sys.argv[1]
     
     while not rospy.is_shutdown():
+        # br.sendTransform((0.0, 0.0, 0.0),
+        #                  (0.0, 0.0, 0.0, 1.0),
+        #                  rospy.Time.now(),
+        #                  robot_map_frame,
+        #                  "world")
+        #
+        # br.sendTransform((0.0, 0.0, 0.0),
+        #                  (0.0, 0.0, 0.0, 1.0),
+        #                  rospy.Time.now(),
+        #                  "world",
+        #                  "map")
+
         br.sendTransform((0.0, 0.0, 0.0),
                          (0.0, 0.0, 0.0, 1.0),
                          rospy.Time.now(),
                          robot_map_frame,
-                         "world")
+                         "map")
         rate.sleep()
